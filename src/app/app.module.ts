1f627166c47691efd9560a682e5d5ab5c8dd2943
evansolomons43 <143550421+evansolomons43@users.noreply.github.com>
2023-09-21 16:01:37 -0400

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -23,7 +24,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
   ],
   bootstrap: [
     AppComponent
